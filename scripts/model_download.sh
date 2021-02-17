dd27438ba4097b017e3e1ca1f09e14b17e5bf3ab
walkernr <walker.nr256@gmail.com>
2021-02-17 15:56:17 -0600

    model download fix

--- scripts/model_download.sh
+++ scripts/model_download.sh
@@ -1,2 +1,4 @@
 cde data download
 wget -P ../model/word2vec 'https://storage.googleapis.com/mat2vec/pretrained_embeddings'
+wget -P ../model/word2vec 'https://storage.googleapis.com/mat2vec/pretrained_embeddings.wv.vectors.npy'
+wget -P ../model/word2vec 'https://storage.googleapis.com/mat2vec/pretrained_embeddings.trainables.syn1neg.npy'
