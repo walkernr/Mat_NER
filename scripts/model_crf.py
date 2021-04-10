07cd12a570e3f55f3c80a250de3a3b29242e49fc
walkernr <walker.nr256@gmail.com>
2021-04-10 16:55:19 -0500

    crf masking fix

--- scripts/model_crf.py
+++ scripts/model_crf.py
@@ -108,7 +108,7 @@ class CRF(nn.Module):
 
     def forward(self, fc_out, tags):
         # mask ignores pad index
-        mask = tags != self.tag_pad_idx
+        mask = torch.where(tags == self.tag_pad_idx, 0,  1)
         # compute output and loss
         crf_out = self.crf.decode(fc_out, mask=mask)
         crf_loss = -self.crf(fc_out, tags=tags, mask=mask, reduction='mean')
