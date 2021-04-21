daf89f50c168da215c19dd6d492513ec3efdc3f6
walkernr <walker.nr256@gmail.com>
2021-04-21 13:33:33 -0500

    full experiments

--- scripts/test.py
+++ scripts/test.py
@@ -65,8 +65,7 @@ n_epoch = 64
 # training proportion
 splits = np.arange(10, 85, 5)
 
-data_names = ['solid_state']
-# data_names = ['aunpmorph', 'doping', 'solid_state']
+data_names = ['aunpmorph', 'doping', 'solid_state']
 
 for data_name in data_names:
     for seed in seeds:
