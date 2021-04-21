leakage fix

scripts/data_utilities.py
@@ -3,121 +3,105 @@ import random
 import numpy as np
 
 
-def data_format(data_path, data_name, sentence_level=True):
-    '''
-    formats the raw data such that each sample is separated into lists of tokens and annotations
-
-    data_path: root directory of the data
-    data_name: name of the data file (assumes json)
-    sentence_level: switch for splitting samples (abstracts) into sentences
-    '''
+def collect_abstracts(data_path, data_name):
+    dois = []
     data = []
     with open(data_path+'/original/'+data_name+'.json', 'r') as f:
         for line in f:
             d = json.loads(line)
+            if d['doi'] in dois:
+                pass
+            else:
+                dois.append(d['doi'])
+                data.append(d)
+    return data
+
+
+def split_abstracts(data, splits, seed):
+    random.Random(seed).shuffle(data)
+    splits = (np.cumsum(splits)*len(data)).astype(np.uint16)
+    test_set = data[:splits[0]]
+    valid_set = data[splits[0]:splits[1]]
+    train_set = data[splits[1]:splits[2]]
+    data_split = {'test': test_set, 'valid': valid_set, 'train': train_set}
+    return data_split
+
+
+def format_abstracts(data_split, seed, sentence_level=True):
+    data_fmt = {key: [] for key in data_split.keys()}
+    for split in data_split.keys():
+        for d in data_split[split]:
             if sentence_level:
                 dat = [{'text': [token['text'] for token in sentence], 'annotation': [token['annotation'] for token in sentence]} for sentence in d['tokens']]
             else:
                 dat = [{key : [token[key] for sentence in d['tokens'] for token in sentence] for key in ['text', 'annotation']}]
-            data.extend(dat)
-    return data
+            data_fmt[split].extend(dat)
+        random.Random(seed).shuffle(data_fmt[split])
+    return data_fmt
 
 
-def data_tag(data, tag_format):
-    '''
-    tags the data
-
-    tag_format: IOB1, IOB2, or IOBES
-    '''
-    for dat in data:
-        annotation = dat['annotation']
-        dat['tag'] = []
-        for i in range(len(annotation)):
-            if tag_format == 'IOB1':
-                if annotation[i] in [None, 'PVL', 'PUT']:
-                    dat['tag'].append('O')
-                elif i == 0:
-                    if annotation[i+1] == annotation[i]:
-                        dat['tag'].append('B-'+annotation[i])
-                    else:
-                        dat['tag'].append('I-'+annotation[i])
-                elif i > 0:
-                    if annotation[i-1] == annotation[i]:
-                        dat['tag'].append('I-'+annotation[i])
-                    else:
+def tag_abstracts(data_fmt, tag_format):
+    for split in data_fmt.keys():
+        for dat in data_fmt[split]:
+            annotation = dat['annotation']
+            dat['tag'] = []
+            for i in range(len(annotation)):
+                if tag_format == 'IOB1':
+                    if annotation[i] in [None, 'PVL', 'PUT']:
+                        dat['tag'].append('O')
+                    elif i == 0:
                         if annotation[i+1] == annotation[i]:
                             dat['tag'].append('B-'+annotation[i])
                         else:
                             dat['tag'].append('I-'+annotation[i])
-            elif tag_format == 'IOB2':
-                if annotation[i] in [None, 'PVL', 'PUT']:
-                    dat['tag'].append('O')
-                elif i == 0:
-                    dat['tag'].append('B-'+annotation[i])
-                elif i > 0:
-                    if annotation[i-1] == annotation[i]:
-                        dat['tag'].append('I-'+annotation[i])
-                    else:
+                    elif i > 0:
+                        if annotation[i-1] == annotation[i]:
+                            dat['tag'].append('I-'+annotation[i])
+                        else:
+                            if annotation[i+1] == annotation[i]:
+                                dat['tag'].append('B-'+annotation[i])
+                            else:
+                                dat['tag'].append('I-'+annotation[i])
+                elif tag_format == 'IOB2':
+                    if annotation[i] in [None, 'PVL', 'PUT']:
+                        dat['tag'].append('O')
+                    elif i == 0:
                         dat['tag'].append('B-'+annotation[i])
-            elif tag_format == 'IOBES':
-                if annotation[i] in [None, 'PVL', 'PUT']:
-                    dat['tag'].append('O')
-                elif i == 0:
-                    if annotation[i+1] == annotation[i]:
-                        dat['tag'].append('B-'+annotation[i])
-                    else:
-                        dat['tag'].append('S-'+annotation[i])
-                elif i > 0 and i < len(annotation)-1:
-                    if annotation[i-1] != annotation[i] and annotation[i+1] == annotation[i]:
-                        dat['tag'].append('B-'+annotation[i])
-                    elif annotation[i-1] == annotation[i] and annotation[i+1] == annotation[i]:
-                        dat['tag'].append('I-'+annotation[i])
-                    elif annotation[i-1] == annotation[i] and annotation[i+1] != annotation[i]:
-                        dat['tag'].append('E-'+annotation[i])
-                    if annotation[i-1] != annotation[i] and annotation[i+1] != annotation[i]:
-                        dat['tag'].append('S-'+annotation[i])
-                elif i == len(annotation)-1:
-                    if annotation[i-1] == annotation[i]:
-                        dat['tag'].append('E-'+annotation[i])
-                    if annotation[i-1] != annotation[i]:
-                        dat['tag'].append('S-'+annotation[i])
-    return data
+                    elif i > 0:
+                        if annotation[i-1] == annotation[i]:
+                            dat['tag'].append('I-'+annotation[i])
+                        else:
+                            dat['tag'].append('B-'+annotation[i])
+                elif tag_format == 'IOBES':
+                    if annotation[i] in [None, 'PVL', 'PUT']:
+                        dat['tag'].append('O')
+                    elif i == 0:
+                        if annotation[i+1] == annotation[i]:
+                            dat['tag'].append('B-'+annotation[i])
+                        else:
+                            dat['tag'].append('S-'+annotation[i])
+                    elif i > 0 and i < len(annotation)-1:
+                        if annotation[i-1] != annotation[i] and annotation[i+1] == annotation[i]:
+                            dat['tag'].append('B-'+annotation[i])
+                        elif annotation[i-1] == annotation[i] and annotation[i+1] == annotation[i]:
+                            dat['tag'].append('I-'+annotation[i])
+                        elif annotation[i-1] == annotation[i] and annotation[i+1] != annotation[i]:
+                            dat['tag'].append('E-'+annotation[i])
+                        if annotation[i-1] != annotation[i] and annotation[i+1] != annotation[i]:
+                            dat['tag'].append('S-'+annotation[i])
+                    elif i == len(annotation)-1:
+                        if annotation[i-1] == annotation[i]:
+                            dat['tag'].append('E-'+annotation[i])
+                        if annotation[i-1] != annotation[i]:
+                            dat['tag'].append('S-'+annotation[i])
+    return data_fmt
 
 
-def data_split(data, splits, seed):
-    '''
-    splits the data into training, validation, and test sets
-
-    data: list of tagged samples
-    splits: (train, validation, test) proportional splits
-    seed: random seed for shuffling
-    '''
-    splits = (np.cumsum(splits)*len(data)).astype(np.uint16)
-    np.random.seed(seed)
-    np.random.shuffle(data)
-    # random.Random(seed).shuffle(data)
-    test_set = data[:splits[0]]
-    valid_set = data[splits[0]:splits[1]]
-    train_set = data[splits[1]:splits[2]]
-    return train_set, valid_set, test_set
-
-
-def data_save(data_path, data_name, alias, train_set, valid_set, test_set):
-    '''
-    formats the raw data such that each sample is separated into lists of tokens and annotations
-    
-    data_path: root directory of the data
-    data_name: name of the data file (assumes json)
-    alias: alias to append to file names
-    train_set: list of training samples
-    valid_set: list of validation samples
-    test_set: list of test samples
-    '''
-    data = [train_set, valid_set, test_set]
-    data_names = [data_name+alias+'_{}.tsv'.format(n) for n in ['train', 'valid', 'test']]
-    for dat, dat_name in zip(data, data_names):
+def save_tagged_splits(data_path, data_name, alias, data_fmt):
+    for split in data_fmt.keys():
+        dat_name = data_name+alias+'_{}.tsv'.format(split)
         with open(data_path+'/split/'+dat_name, 'w') as f:
-            for d in dat:
+            for d in data_fmt[split]:
                 for txt, tg in zip(d['text'], d['tag']):
                     f.write('{}\t{}\n'.format(txt, tg))
-                f.write('\n')              
+                f.write('\n')

scripts/test.py
@@ -7,7 +7,7 @@ from torch import nn
 from torch.optim import Adam
 from torchtools.optim import RangerLars
 from seqeval.metrics import classification_report
-from data_utilities import data_format, data_tag, data_split, data_save
+from data_utilities import collect_abstracts, split_abstracts, format_abstracts, tag_abstracts, save_tagged_splits
 from data_tokenizer import MaterialsTextTokenizer
 from data_corpus import DataCorpus
 from model_crf import CRF
@@ -20,7 +20,7 @@ m = 80
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-seeds = [2**x for x in np.arange(8, 16)]
+seeds = [2**x for x in np.arange(16)]
 torch.backends.cudnn.deterministic = True
 
 new_calculation = True
@@ -65,8 +65,7 @@ n_epoch = 64
 # training proportion
 splits = np.arange(10, 85, 5)
 
-data_names = ['aunpmorph', 'solid_state']
-# data_names = ['solid_state']
+data_names = ['aunpmorph', 'doping', 'solid_state']
 
 for data_name in data_names:
     for seed in seeds:
@@ -96,8 +95,8 @@ for data_name in data_names:
         configs = {'_crf_iobes_{}_{}'.format(seed, split): {'sentence_level': True, 'format': 'IOBES', 'use_crf': True, 'lr': 5e-2, 'split': (0.1, split/800, split/100)} for split in splits}
                 
         for alias, config in configs.items():
-            data = data_tag(data_format(data_path, data_name, config['sentence_level']), tag_format=config['format'])
-            data_save(data_path, data_name, '_{}'.format(seed), *data_split(data, config['split'], seed))
+            data = tag_abstracts(format_abstracts(split_abstracts(collect_abstracts(data_path, data_name), config['split'], seed), seed, config['sentence_level']), config['format'])
+            save_tagged_splits(data_path, data_name, '_{}'.format(seed), data)
             corpus = DataCorpus(data_path=data_path, data_name=data_name, alias='_{}'.format(seed), vector_path=vector_path,
                                 tokenizer=tokenizer, cased=cased, tag_format=config['format'], batch_size=batch_size, device=device)
 
@@ -119,7 +118,7 @@ for data_name in data_names:
 
             print('train set: {} sentences'.format(len(corpus.train_set)))
             print('valid set: {} sentences'.format(len(corpus.valid_set)))
-            print('test set: {} sentences'.format(len(corpus.valid_set)))
+            print('test set: {} sentences'.format(len(corpus.test_set)))
             print(m*'-')
 
             text_pad_idx = corpus.text_pad_idx